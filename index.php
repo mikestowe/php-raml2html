173b7e9e1017c1948c87edf849432cfa178d4cf3
Michael Stowe <me@mikestowe.com>
2014-05-27 11:46:38 -0700

    Added Credits and License Info
    Shameless self promotion…  oh and a link back to the page so you can
    watch for updates

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+/**
+  * RAML2HTML for PHP -- A Simple API Docs Script for RAML & PHP
+  * @version 0.1beta
+  * @author Mike Stowe <me@mikestowe.com>
+  * @link https://github.com/mikestowe/php-raml2html
+  * @link http://www.mikestowe.com/2014/05/raml-2-html.php
+  * @license http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+  */
+
 require_once('inc/spyc.php');
 require_once('config.php');
 
